0bcd19db68154aa56e0fac39722ecc03d67b22d3
Kristina Sharaeva <224787@niuitmo.ru>
2022-03-23 22:31:51 +0300

    Base SQLAlchemy logic added:
     - In memory (just for now) database created
     - Class Hero with it's properties implemented
     - Class Story to store heroes short story implemented

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
-print('here will be models')
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Hero(Base):
+    __tablename__ = 'heroes'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    birthday = Column(String)
+    tribe = Column(String)
+    side = Column(String, nullable=False)
+    power = Column(Integer)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}:{self.id}:{self.name}'
+
+
+class Story(Base):
+    __tablename__ = 'stories'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    story = Column(String, unique=True)
+    hero_id = Column(Integer, ForeignKey('heroes.id'))
+
--- repositories.py
+++ repositories.py
@@ -1,1 +1,16 @@
-print('here will be project')
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base, Hero
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Eloy = Hero(name='Eloy', tribe='Nora', power=0, side='WhiteCarcha')
+session.add(Eloy)
+session.commit()
+
+print(session.query(Hero).get(1))
